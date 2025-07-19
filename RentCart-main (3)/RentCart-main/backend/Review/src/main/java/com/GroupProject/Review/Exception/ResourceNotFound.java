d1950dad68adabea78429cf8a10a0e2a9cf14f4f
kartiksaste123 <kartiksaste68@gmail.com>
2025-07-20 00:48:02 +0530

    Remove all folders and files from repository

--- RentCart-main (3)/RentCart-main/backend/Review/src/main/java/com/GroupProject/Review/Exception/ResourceNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.GroupProject.Review.Exception;
-
-public class ResourceNotFound extends RuntimeException {
-    public ResourceNotFound(String message) {
-        super(message);
-    }
-}
